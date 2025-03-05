e84b8fde97ea7282446554b7224d8d21715eac29
eeswepe <singgihwahyupermana@gmail.com>
2025-03-05 09:23:36 +0700

    not use string.h code

--- day2/solve.c
+++ day2/solve.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-#include <string.h>
 
 int word_score(const char *word) {
   int sum = 0;
-  for (int i = 0; i < strlen(word); i++) {
-    sum += word[i] - 'a' + 1;
+  while (*word) {
+    sum += *word - 'a' + 1;
+    word++;
   }
   return sum;
 }
