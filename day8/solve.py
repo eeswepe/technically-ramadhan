53c823239238905d3ad8aa1987d44d21024fba30
eeswepe <singgihwahyupermana@gmail.com>
2025-03-10 20:53:33 +0700

    yang lain mana min

---
+++ day8/solve.py
@@ -0,0 +1,27 @@
+def increment_string(strng):
+    if len(strng) == 0:
+        return "1"
+
+    if strng.isdigit():
+        return str(int(strng) + 1)
+
+    marker = len(strng)
+
+    for i in range(len(strng) - 1, -1, -1):
+        if not strng[i].isdigit():
+            marker = i + 1
+            break
+
+    angka = strng[marker:]
+
+    # Jika tidak ada angka di akhir string, tambahkan "1"
+    if angka == "":
+        return strng + "1"
+
+    if marker == 0:
+        return str(int(angka) + 1).zfill(len(angka))
+
+    return strng[:marker] + str(int(angka) + 1).zfill(len(angka))
+
+
+print(increment_string("foo"))
